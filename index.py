225d0eade4f79da9161ebb2591d13c18e9c7b7a0
nisamozr <nisamudheen0z6@gmail.com>
2020-06-28 09:58:14 +0530

    caluculater starting

---
+++ index.py
@@ -0,0 +1,20 @@
+from  tkinter import *
+
+window = Tk()
+window.title("HTML to PDF Converter")
+window.geometry("400x500")
+window.config(bg='#b4f0df')
+
+
+def equation(args):
+    pass
+
+
+input_field = Entry(window, textvariable=equation)
+input_field.place(height=50,widt=400)
+#butons
+
+
+
+
+window.mainloop()
